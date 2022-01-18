acb38cf794553f1f1ff27a6b394d1b8f2668b4b4
fabiokleis <fabiohkrc@gmail.com>
2022-01-18 18:49:57 -0300

    commented some tests in server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+//const dotenv = require('dotenv').config();
 const userRouter = require('./routes/userRoute.js');
 const db = require('./infra/database.js');
 const app = express();
 
+/*
 async function testConnection() {
     const cn = await db.connect();
     cn.done();
@@ -15,7 +16,7 @@ try {
 } catch (err) {
     throw err;
 }
-
+*/
 app.use('/user', userRouter);
 
 app.use((err, req, res, next) => {
